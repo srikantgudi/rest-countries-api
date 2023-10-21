bdbf24e320ec6e31c711155a32a01e52fb436e0d
Srikant Gudi <srikantgudi@gmail.com>
2023-10-21 11:50:59 +0530

    Update +page.svelte

--- src/routes/[code]/+page.svelte
+++ src/routes/[code]/+page.svelte
@@ -3,27 +3,19 @@
   export let data;
 </script>
 
-<style>
-  .grid {
-    display: grid;
-    grid-template-columns: 24rem 1fr;
-    gap: 1rem;
-  }
-</style>
-
 <main class={`${$mode}`}>
   <div class="w-3/4 mx-auto">
     <div class="mb-8">
       <a href="/">&leftarrow; Back to list</a>
     </div>
-    <div class="grid">
+    <div class="grid md:grid-cols-2 grid-cols-1">
       <div>
         <div class="text-3xl">{data.row.name}</div>
         <figure>
           <img src={data.row.flags.png} alt="flag" />
         </figure>
       </div>
-      <div>
+      <div class="mt-4">
         <div>Population: {data.row.population}</div>
         <div>Region: {data.row.region}</div>
         <div>Capital: <strong>{data.row.capital}</strong></div>
