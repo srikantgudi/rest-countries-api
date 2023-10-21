1ebf224bc4e679c78bc5320e2a9c4feef3583c7e
Srikant Gudi <srikantgudi@gmail.com>
2023-10-21 10:57:58 +0530

    modified

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,23 @@
 <script>
   import data from '../data/data.json';
+  import {mode} from '../store';
+
   let searchRegion = '';
   let searchCountry = '';
   let regions = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
-  let mode = 'lite';
   let modeLabel = 'Lite';
+  let textColor = 'black';
 
   $: filterRow = (f) => ((regions.includes(searchRegion) ? f.region.includes(searchRegion) : true) 
       && (searchCountry ? f.name.toLowerCase().includes(searchCountry.toLowerCase()) : true));
-  $: modeLabel = mode === 'dark' ? 'Lite' : 'Dark';
+  $: modeLabel = $mode === 'dark' ? 'Lite' : 'Dark';
 
   const toggleMode = () => {
-    mode = mode == 'dark' ? 'lite' : 'dark';
+    $mode = ($mode === "dark" ? "lite" : "dark");
+    textColor = ($mode === "dark" ? "white" : "black");
+    var element = document.body;
+   element.classList.toggle("dark");
+   element.style.height = "100%";
   }
 </script>
 
@@ -22,11 +28,6 @@
     align-items: center;
     gap: 1rem;
     margin-bottom: 1rem;
-  }
-  .countries {
-    box-shadow: 0 0 4px #999;
-    height: 80vh;
-    overflow-y: auto;
   }
   .countries-grid {
     display: flex;
@@ -39,18 +40,6 @@
   img {
     background-size: contain;
   }
-  figure {
-    display:flex;
-    align-items:center;
-    height:12rem;
-  }
-  .dark {
-    background-color: hsl(209, 23%, 22%);
-    color: antiquewhite;
-  }
-  .lite {
-    background-color: hsl(0, 0%, 100%);    
-  }
   @media screen and (max-width:480px) {
     nav {
       flex-direction: column;
@@ -59,42 +48,49 @@
   }
 </style>
 
-<main class={mode}>
-  <nav class="">
-    <h1 class="text-3xl font-bold">Where in the world?</h1>
-    <button class="bg-{mode=='dark' ? 'gray' : 'white'}-700 border text-{mode=='dark'?'white':'gray'} px-4 p-1 rounded-md" on:click={toggleMode}>{modeLabel}</button>
-  </nav>
-  <nav class="px-4">
+<main class={`${$mode}`}>
+  <div class="w-3/4 mx-auto h-full">
+    <nav>
+      <h1 class="text-3xl font-bold">Where in the world?</h1>
+      <button class="bg-{$mode=='dark' ? 'gray' : 'white'}-700 border text-{$mode=='dark'?'white':'gray'} px-4 p-1 rounded-md" on:click={toggleMode}>
+        {#if $mode=='dark'}
+        <i class="fa fa-sun"></i> {modeLabel}
+        {:else}
+        <i class="fa fa-moon"></i> {modeLabel}
+        {/if}
+      </button>
+    </nav>
+    <nav>
+      <div class="border p-2 w-[24rem]">
+        <i class="fa fa-search px-2"></i>
+        <input type="search" bind:value={searchCountry} class={`outline-0 p-1 dark:bg-[hsl(209,209,209)] dark:text-white`} />
+      </div>
+      <div class="text-{textColor}">
+        <select bind:value={searchRegion} class="border-2 p-1 text-{$mode==="dark" ? "white" : "hsl(209,209,209)"} bg-{$mode==='dark'? 'slate-900': 'white'} relative rounded-md">
+          {#if !searchRegion}<option value="">- Filter by Region -</option>{/if}
+          <option value="">All</option>
+          {#each regions as rgn}
+          <option value={rgn}>{rgn}</option>
+          {/each}
+        </select>
+      </div>
+    </nav>
     <div>
-      <input type="search" bind:value={searchCountry} class="border-2 p-1 rounded-md" />
-    </div>
-    <div>
-      <select bind:value={searchRegion} class="border-2 p-1 rounded-md">
-        {#if !searchRegion}<option value="">- Filter by Region -</option>{/if}
-        <option value="">All</option>
-        {#each regions as rgn}
-        <option value={rgn}>{rgn}</option>
-        {/each}
-      </select>
-    </div>
-  </nav>
-  <div>
-    <div class="flex flex-wrap justify-around">
-      {#each data.filter(f => filterRow(f)) as row}
-        <a href={`/${row.alpha3Code}`} class="mb-4 p-4">
-          <div class="border-2 rounded-md md:p-1 md:pb-4 pb-1 md:w-[16vw] w-full flex flex-col items-center">
-            <figure class="mb-4">
-              <img height="100" src={row.flags.png} alt="flag" />
-            </figure>
-            <div>
-              <div class="md:text-2xl">{row.name}</div>
-              <div>Population: {row.population}</div>
-              <div>Region: {row.region}</div>
-              <div>Capital: {row.capital}</div>
+      <div class="flex md:flex-row md:flex-wrap flex-col justify-between gap-1">
+        {#each data.filter(f => filterRow(f)) as row}
+          <a href={`/${row.alpha3Code}`} class="mb-4 flex flex-wrap items-between justify-around">
+            <div class="rounded-t-lg pb-1 md:w-[18rem] w-full shadow-md">
+              <img height="100" src={row.flags.png} alt="flag" class="object-top rounded-t-lg mb-4 h-[12rem] w-full" />
+              <div class="p-1 px-8 leading-md">
+                <div class="md:text-2xl">{row.name}</div>
+                <div>Population: {row.population}</div>
+                <div>Region: {row.region}</div>
+                <div>Capital: {row.capital}</div>
+              </div>
             </div>
-          </div>
-        </a>
-      {/each}
+          </a>
+        {/each}
+      </div>
     </div>
   </div>
 </main>
--- src/routes/[code]/+page.svelte
+++ src/routes/[code]/+page.svelte
@@ -1,11 +1,9 @@
 <script>
+  import {mode} from '../../store';
   export let data;
 </script>
 
 <style>
-  .border-link {
-    margin: auto 1rem;
-  }
   .grid {
     display: grid;
     grid-template-columns: 24rem 1fr;
@@ -13,30 +11,32 @@
   }
 </style>
 
-<main>
-  <div>
-    <a href="/">Back to list</a>
-  </div>
-  <hr>
-  <div class="grid">
-    <div>
-      <h2>{data.row.name}</h2>
-      <figure>
-        <img src={data.row.flags.png} alt="flag" />
-      </figure>
+<main class={`${$mode}`}>
+  <div class="w-3/4 mx-auto">
+    <div class="mb-8">
+      <a href="/">&leftarrow; Back to list</a>
     </div>
-    <div>
-      <div>Population: {data.row.population}</div>
-      <div>Region: {data.row.region}</div>
-      <div>Capital: <strong>{data.row.capital}</strong></div>
-      <div>Alpha2Code: {data.row.alpha2Code}</div>
-      <div>Alpha3code: {data.row.alpha3Code}</div>
-      <div>Sub-Region: {data.row.subregion}</div>
-      <div>Borders:</div>
-      <div class="d-flex gap-4">
-        {#each data.row.borders as item}
-        <a href={`/${item}`} class="card px-2">{item}</a>
-        {/each}
+    <div class="grid">
+      <div>
+        <div class="text-3xl">{data.row.name}</div>
+        <figure>
+          <img src={data.row.flags.png} alt="flag" />
+        </figure>
+      </div>
+      <div>
+        <div>Population: {data.row.population}</div>
+        <div>Region: {data.row.region}</div>
+        <div>Capital: <strong>{data.row.capital}</strong></div>
+        <div>Alpha2Code: {data.row.alpha2Code}</div>
+        <div>Alpha3code: {data.row.alpha3Code}</div>
+        <div>Sub-Region: {data.row.subregion}</div>
+        <div>Borders:</div>
+        <div class="flex gap-4 mt-2">
+          {#each data.row.borders as item}
+          <a href={`/${item}`} class="border rounded hover:bg-slate-300 p-1 px-2">{item}</a>
+          {/each}
+        </div>
       </div>
     </div>
+  </div>
 </main>
